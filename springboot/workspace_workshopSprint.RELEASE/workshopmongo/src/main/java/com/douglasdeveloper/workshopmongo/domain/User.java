cb1574daeb82f37ed3cdced09b4b280b76a2417d
douglas.developer <douglassbarbosa22@gmail.com>
2021-01-09 18:16:02 -0300

    Connect MongoDB
    I used to connect Repository and Service

--- springboot/workspace_workshopSprint.RELEASE/workshopmongo/src/main/java/com/douglasdeveloper/workshopmongo/domain/User.java
+++ springboot/workspace_workshopSprint.RELEASE/workshopmongo/src/main/java/com/douglasdeveloper/workshopmongo/domain/User.java
@@ -2,9 +2,14 @@ package com.douglasdeveloper.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
@@ -40,7 +45,6 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	
 	@Override
 	public int hashCode() {
@@ -66,8 +70,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-
-	
-	
+		
 }
--- springboot/workspace_workshopSprint.RELEASE/workshopmongo/src/main/java/com/douglasdeveloper/workshopmongo/resources/UserResource.java
+++ springboot/workspace_workshopSprint.RELEASE/workshopmongo/src/main/java/com/douglasdeveloper/workshopmongo/resources/UserResource.java
@@ -1,25 +1,26 @@
 package com.douglasdeveloper.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglasdeveloper.workshopmongo.domain.User;
+import com.douglasdeveloper.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
-	@RequestMapping(method=RequestMethod.GET)
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria Brown", "mariabrown@gmail.com");
-		User alex = new User("2", "Alex Green", "alexgreen@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
