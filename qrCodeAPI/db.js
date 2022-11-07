57a9b35ab622f3b05346c0e1ce540021684d3507
douglas.dev0111 <douglassbarbosa22@gmail.com>
2022-11-07 20:25:00 -0300

    update project

--- qrCodeAPI/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const componenteSequelize = new Sequelize('systemqrcode', 'douglas', 'minhasenha', {
- dialect: 'mysql',
- host: '127.0.0.1',
- port: 3306
-});
-
-module.exports = componenteSequelize;
