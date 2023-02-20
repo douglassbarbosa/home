eb3b81f642768a41f2729224fddbaac65e09d292
Douglas Barbosa <douglassbarbosa22@gmail.com>
2023-02-20 17:51:45 -0300

    refactor: New Code

--- C#/aplicacoes/Calculator/Program.cs
+++ C#/aplicacoes/Calculator/Program.cs
@@ -7,49 +7,154 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Enter a value 1: ");
-            var number = float.Parse(Console.ReadLine());
 
+            Menu();
+        }
+        static void Sum()
+        {
             Console.Clear();
-            Console.WriteLine("Enter a operator: ");
-            var op = Console.ReadLine();
+            Console.WriteLine("Enter a value 1: ");
+            var number = double.Parse(Console.ReadLine());
 
             Console.Clear();
             Console.WriteLine("Enter a value 2: ");
-            var number2 = float.Parse(Console.ReadLine());
+            var number2 = double.Parse(Console.ReadLine());
 
             Console.Clear();
-            switch (op)
+            Console.WriteLine("");
+            Console.WriteLine("Result " + number + " + " + number2 + " = " + (number + number2));
+
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtraction()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a value 1: ");
+            var number = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Enter a value 2: ");
+            var number2 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Result " + number + " - " + number2 + " = " + (number - number2));
+
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multip()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a value 1: ");
+            var number = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Enter a value 2: ");
+            var number2 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Result " + number + " * " + number2 + " = " + (number * number2));
+
+            Console.ReadKey();
+            Menu();
+        }
+        static void Division()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a value 1: ");
+            var number = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Enter a value 2: ");
+            var number2 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            if ((int)number2 == 0)
             {
-                case "+":
-                    Console.WriteLine("Resultado: " + (number + number2));
-                    break;
-                case "-":
-                    Console.WriteLine("Resultado: " + (number - number2));
-                    break;
-                case "*":
-                    Console.WriteLine("Resultado: " + (number * number2));
-                    break;
-                case "/":
-                    if ((int)number2 == 0)
-                    {
-                        Console.WriteLine("ERROR - Don't divisions for Zero");
-                        break;
-                    }
-                    Console.WriteLine("Resultado: " + (number / number2));
-                    break;
-                case "%":
-                    Console.WriteLine("Resultado: " + (number % number2));
-                    break;
-                case "^":
-                    Console.WriteLine("Resultado: " + (Math.Pow(number, number2)));
-                    break;
+                Console.WriteLine("ERROR - Don't divisions for Zero");
+
+                Console.ReadKey();
+                Menu();
+            }
+            Console.WriteLine("Result " + number + " / " + number2 + " = " + (number / number2));
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Module()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a value 1: ");
+            var number = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Enter a value 2: ");
+            var number2 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            if ((int)number2 == 0)
+            {
+                Console.WriteLine("ERROR - Don't divisions for Zero");
+
+                Console.ReadKey();
+                Menu();
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Result " + number + " % " + number2 + " = " + (number % number2));
+
+            Console.ReadKey();
+            Menu();
+        }
+        static void Power()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a value 1: ");
+            var number = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("Enter a value 2: ");
+            var number2 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Result " + number + " ^ " + number2 + " = " + (Math.Pow(number, number2)));
+            Console.ReadKey();
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Module");
+            Console.WriteLine("6. Power");
+            Console.WriteLine("0. Quit");
+            Console.WriteLine("");
+            Console.WriteLine(" ----------- ");
+            Console.WriteLine("");
+            Console.WriteLine("Enter your choice: ");
+
+            short choice = short.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Multip(); break;
+                case 4: Division(); break;
+                case 5: Module(); break;
+                case 6: Power(); break;
+                case 0: System.Environment.Exit(0); break;
                 default:
-                    Console.WriteLine("Invalid operator. Try Again!");
+                    Console.WriteLine("Invalid choice. Try Again!");
                     break;
             }
-            Console.WriteLine();
-            Console.ReadKey();
+
         }
     }
 }
