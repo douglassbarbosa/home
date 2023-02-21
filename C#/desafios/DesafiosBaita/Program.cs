b71da2ef62dceab6bb151eaae502e237b594150d
Douglas Barbosa <douglassbarbosa22@gmail.com>
2023-02-21 11:37:50 -0300

    feat: Add Desafio 2

--- C#/desafios/DesafiosBaita/Program.cs
+++ C#/desafios/DesafiosBaita/Program.cs
@@ -37,11 +37,55 @@ namespace DesafiosBaita
             Console.ReadKey();
             MenuDesafios();
         }
+
+
+        static void DesafioTwo()
+        {
+            Console.Clear();
+            double altura = 0.00f;
+            double peso = 0.00f;
+            double massaCorporal = 0.00f;
+            double imc = 0.00f;
+
+            Console.WriteLine("Informe sua altura (com vírgula): ");
+            altura = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe seu peso: ");
+            peso = Double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            double CalculaIndice(double altura, double peso)
+            {
+                return massaCorporal = peso / (altura * altura);
+            }
+
+            (string classificacao, string risco) StatusIMC(double imc)
+            {
+                if (imc < 16) return ("Magreza Grau III", "N/A");
+                if (imc < 17) return ("Magreza Grau II", "N/A");
+                if (imc < 18.5) return ("Magreza Grau I", "N/A");
+                if (imc < 25) return ("Eutrofia", "N/A");
+                if (imc < 30) return ("Sobrepeso", "Aumentado");
+                if (imc < 35) return ("Obesidade Grau I", "Moderado");
+                if (imc <= 40) return ("Obesidade Grau II", "Grave");
+                return ("Obesidade Grau III", "Muito Grave");
+            }
+
+            imc = CalculaIndice(altura, peso);
+            (var classificacao, var risco) = StatusIMC(imc);
+            Console.WriteLine($"O IMC calculado para a altura de {string.Format("{0:N}", altura)}m e o peso de {peso}kg é:");
+            Console.WriteLine("");
+            Console.WriteLine($"Seu IMC é {string.Format("{0:N}", imc)} kg/m²");
+            Console.WriteLine($"Classificação: {classificacao}");
+            Console.WriteLine($"Risco: {risco}");
+
+            Console.ReadKey();
+            MenuDesafios();
+        }
         static void MenuDesafios()
         {
             Console.Clear();
             Console.WriteLine("1. Desafio 1");
-            // Console.WriteLine("2. Desafio ");
+            Console.WriteLine("2. Desafio 2");
             // Console.WriteLine("3. Desafio ");
             // Console.WriteLine("4. Desafio ");
             Console.WriteLine("0. Sair");
@@ -54,7 +98,7 @@ namespace DesafiosBaita
             switch (choice)
             {
                 case 1: DesafioOne(); break;
-                // case 2: ; break;
+                case 2: DesafioTwo(); break;
                 // case 3: ; break;
                 // case 4: ; break;
                 // case 5: ; break;
