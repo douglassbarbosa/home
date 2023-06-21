f1c743d5097e37c8a2de6f08bab4963131c932f6
Douglas Barbosa <douglassbarbosa22@gmail.com>
2023-06-20 21:33:04 -0300

    feat: desafio 1000 em C#

--- BoxOfCodes/UriOnlineJudge/1000.cs
+++ BoxOfCodes/UriOnlineJudge/1000.cs
@@ -2,8 +2,5 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    static void Main() => System.Console.WriteLine("Hello World!");
 }
