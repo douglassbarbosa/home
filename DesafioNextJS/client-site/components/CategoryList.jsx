dd4234676bd6c70217c615c72cc40a9228c599f5
Douglas Barbosa <douglassbarbosa22@gmail.com>
2023-07-25 05:27:24 -0300

    fix: Loading page Category

--- DesafioNextJS/client-site/components/CategoryList.jsx
+++ DesafioNextJS/client-site/components/CategoryList.jsx
@@ -1,52 +1,26 @@
-"use client"
-import { useState, useEffect } from "react";
-import Link from "next/link";
-import RemoveBtn from "./RemoveBtn";
-import { HiPencilAlt } from 'react-icons/hi/index';
-
-const getData = async () => {
-    try {
-        const datas = await fetch('http://localhost:5271/api/CategoriaCliente', {
-            cache: "no-store",
-        });
-        
-        if (!datas.ok) {
-            throw new Error('Failed to fetch topics');
-        }
-        return datas.json();
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return []; // Retorna um array vazio em caso de erro.
+async function getData() {
+    const res = await fetch('http://localhost:5271/api/CategoriaCliente')
+   
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data, contact your administrator Douglas@gmail.com');
     }
-}
-
-export default function CategoryList() {
-    const [categorias, setCategorias] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await getData();
-            setCategorias(data);
-        };
-
-        fetchData();
-    }, []);
-
+    
+    return res.json()
+  }
+   
+  export default async function CategoryList() {
+    const categorias = await getData()
+   
     return (
-        <>
-            {categorias.map((t) => (
-                <div key={t._id} className="p4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-                    <div>
-                        <h3>{t.nome}</h3>
-                    </div>
-                    <div className="flex gap-2">
-                        <RemoveBtn />
-                        <Link href={`/editTopic/${t._id}`}>
-                            <HiPencilAlt size={24} />
-                        </Link>
-                    </div>
-                </div>
+        <main>
+            <h3>Categorias:</h3>
+          {}
+          <ul className="category-list">
+            {categorias.map((categoria) => (
+              <li key={categoria.id} className="category-item">{categoria.nome}</li>
             ))}
-        </>
-    )
-}
+          </ul>
+        </main>
+      );
+  }
