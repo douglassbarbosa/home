7f1228a87d0fded8ebe7f913d61b71c4044eb047
Douglas Barbosa <douglassbarbosa22@gmail.com>
2023-07-25 06:44:13 -0300

    chore: add pre list Category

--- DesafioNextJS/WebAPI/Models/CategoriaClienteContext.cs
+++ DesafioNextJS/WebAPI/Models/CategoriaClienteContext.cs
@@ -8,6 +8,23 @@ namespace WebAPI.Models
         public CategoriaClienteContext(DbContextOptions<CategoriaClienteContext> options)
             : base(options)
         {
+            if (!CategoriaCliente.Any())
+            {
+                // Cria uma lista de Categorias pré-carregadas
+                List<CategoriaCliente> categorias = new List<CategoriaCliente>
+                {
+                    new CategoriaCliente { Nome = "Categoria A" },
+                    new CategoriaCliente { Nome = "Categoria B" },
+                    new CategoriaCliente { Nome = "Categoria C" },
+                    new CategoriaCliente { Nome = "Categoria D" },
+                    new CategoriaCliente { Nome = "Categoria E" },
+                    // Adicione mais categorias conforme necessário
+                };
+
+                // Adiciona as categorias no contexto e salva as mudanças no banco de dados
+                CategoriaCliente.AddRange(categorias);
+                SaveChanges();
+            }
         }
 
         public DbSet<CategoriaCliente> CategoriaCliente { get; set; } = null!;
