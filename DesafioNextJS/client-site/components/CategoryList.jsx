38c717a1089033f3d8c025a99d42efea7d495061
Douglas Barbosa <douglassbarbosa22@gmail.com>
2023-08-19 16:46:42 -0300

    feat: implementating popup

--- DesafioNextJS/client-site/components/CategoryList.jsx
+++ DesafioNextJS/client-site/components/CategoryList.jsx
@@ -1,49 +1,67 @@
-import Link from 'next/link'; 
-import AddCategory from '../components/AddCategory'
-import RemoveBtn from "./RemoveBtn";
+'use client'
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import AddCategory from '../components/AddCategory';
+import RemoveBtn from './RemoveBtn';
 
 
 
-async function getData() {
-  const res = await fetch('http://localhost:5271/api/CategoriaCliente', { cache: 'no-store' })
+export default function CategoryList() {
+  const [_categorias, setCategorias] = useState([]);
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data, contact your administrator Douglas@gmail.com');
+  async function getData() {
+    const res = await fetch('http://localhost:5271/api/CategoriaCliente', { cache: 'no-store' });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data, contact your administrator Douglas@gmail.com');
+    }
+
+    return res.json();
   }
+  useEffect(() => {
+ 
+    const fetchData = async () => {
+      try {
 
-  return res.json()
-}
+        const categoriasData = await getData();
+        setCategorias(categoriasData); 
 
-export default async function CategoryList() {
-  const categorias = await getData()
+        console.log('Dados obtidos:', categoriasData);
+      } catch (error) {
+        console.error('Erro ao obter dados:', error);
+      }
+    };
+
+    fetchData();
+  }, [_categorias]);
 
   return (
     <main>
-      <div className='category-add'>        
-      <AddCategory />
+      <div className='category-add'>
+        <AddCategory />
       </div>
-      <h3>Categorias:</h3> 
+      <h3>Categorias:</h3>
       <div>
         <div>
           <ul className="category-list">
-            {
-            categorias.map((categoria) => (
-            <li key={categoria.id} className="category-item">
-              {categoria.nome}
-              <div className="button-container">
-                <Link href={`./edit/${categoria.id}`} className="edit-button">
-                  EDITAR
-                </Link>                 
-                <RemoveBtn id={categoria.id} /> 
-              </div>
-            </li>
-            ))}   
+            {_categorias.map((categoria) => (
+              <li key={categoria.id} className="category-item">
+
+                {categoria.nome}
+
+                <div className="button-container">
+
+                  <Link href={`/edit/${categoria.id}`} className="edit-button">
+                    EDITAR
+                  </Link>
+                  <RemoveBtn id={categoria.id} />
+
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
-      
-      
     </main>
   );
 }
